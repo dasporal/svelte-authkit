6e8af60ddc8da2ff39955ee552138668f707e11a
dasporal <dasporal@gmail.com>
2024-01-11 15:15:46 +0100

    removed store

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,4 @@ import SignInButton from "./components/SignInButton.svelte";
 import SignOutButton from "./components/SignOutButton.svelte";
 
 export { SignInButton, SignOutButton}
-export {default as userStore} from "./stores/userStore.js"
 export {default as SvelteAuthKit } from "./components/SvelteAuthKit.svelte";
